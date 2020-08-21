59d3a9d7f7797e25e1fa846d6ca1d29ccbe24b0f
kay-22 <try1.zipzap@gmail.com>
2020-08-21 20:56:40 +0300

    now using SPI_SETMOUSESPEED instead msdn example didn't work

--- main.c
+++ main.c
@@ -2,39 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//msdn example:
-//https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfoa?redirectedfrom=MSDN#examples
+//https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfoa
 
 
-#define ARG_SPEED_INDEX 1
-#define SYS_SPEED_INDEX 2
+#define SPEED_INDEX 1
 #define ARGS            2
 
 int main (int argc, char ** argv)
 {
     if (argc != ARGS) 
     {
-        printf(stderr, "Error: mspeed only takes one argument. Try 'mspeed <speed>'. speed must be between 0 and 20.");
+        fprintf(stderr, "Error: mspeed only takes one argument. Try 'mspeed <speed>'. speed must be between 0 and 20.");
         return EXIT_FAILURE;
     }
     
-    int new_speed = atoi(argv[ARG_SPEED_INDEX]);
+    int new_speed = atoi(argv[SPEED_INDEX]);
     if (new_speed < 0 || new_speed > 20) 
     {
-        printf(stderr, "Error: invalid mouse speed -- must be between 0 and 20");
+        fprintf(stderr, "Error: invalid mouse speed -- must be between 0 and 20");
         return EXIT_FAILURE;
     }
 
-    int data[3] = {0};
-    if (SystemParametersInfo(SPI_GETMOUSE, 0, &data,0)) 
+    if (!SystemParametersInfo(SPI_SETMOUSESPEED, 0, (LPVOID)(intptr_t)new_speed, SPIF_UPDATEINIFILE | 
+                                                                       SPIF_SENDCHANGE | 
+                                                                       SPIF_SENDWININICHANGE))
     {
-        data[SYS_SPEED_INDEX] = new_speed;
-    }
-    else 
-    {   
-        printf(stderr, "Error: could not get mouse data");
+        fprintf(stderr, "Error: could not set new mouse speed");
         return EXIT_FAILURE;
     }
-
-    SystemParametersInfo(SPI_SETMOUSE, 0, data, SPIF_SENDCHANGE);
 }
